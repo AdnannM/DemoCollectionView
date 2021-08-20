1e3057a4b5790cb10506b4365fa26761b22c5c28
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-08-20 09:49:48 +0200

    Initial Commit

---
+++ DemoCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoCollectionView
+//
+//  Created by Adnann Muratovic on 20.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
